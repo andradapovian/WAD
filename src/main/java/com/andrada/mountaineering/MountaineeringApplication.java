79215651e0a9e41aea82e4b5aab8f77cccad219f
andradapovian <andrada.povian98@gmail.com>
2019-10-19 14:21:21 +0300

    first commit

---
+++ src/main/java/com/andrada/mountaineering/MountaineeringApplication.java
@@ -0,0 +1,13 @@
+package com.andrada.mountaineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MountaineeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MountaineeringApplication.class, args);
+	}
+
+}
